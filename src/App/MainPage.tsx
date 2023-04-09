d809b01930cee4b1ce144fcc93f2f0e4acd91da0
jogerj <30559735+jogerj@users.noreply.github.com>
2023-04-09 19:11:06 +0200

    Basic tests

--- src/App/MainPage.tsx
+++ src/App/MainPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { FileUploader } from "react-drag-drop-files";
 import { Dropdown, Button } from "react-bootstrap";
 import DropdownItem from "react-bootstrap/DropdownItem"
